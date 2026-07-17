89b557e95e48e993f34207caa9dd9b8c38d99a64
Murad Hasan Tamim <mhtamim136@gmail.com>
2026-07-17 20:06:05 +0600

    Update apiBaseUrl to a new endpoint

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.TASHE_CONFIG = {
   appName: 'TaShe Cloud',
   rootFolderId: '1IRiIh1MOfjJkGAjlj-ig2YD0QBn0pe29',
   securePassword: '1663136',
-  apiBaseUrl: localStorage.getItem('tashe.apiBaseUrl') || '',
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbwBOTXO7On4PvwpoEug3oQmcJDmUaZ-s296US7CD9J1CeHz0PG1BjX9LIsQ3gpzQ3gA4A/exec',
   maxUploadBytes: 45 * 1024 * 1024,
   websiteBaseUrl: location.origin + location.pathname
 };
